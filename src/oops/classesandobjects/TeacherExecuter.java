2b08ad4aa14e787ce6e7f3d6bbcad0ef994c569b
hk1997 <khandelwal.hardik14@gmail.com>
2022-08-25 22:40:23 +0530

    Teacher executer class

---
+++ src/oops/classesandobjects/TeacherExecuter.java
@@ -0,0 +1,24 @@
+package oops.classesandobjects;
+
+public class TeacherExecuter {
+
+    public static void main(String[] args) {
+
+        Teacher satyamSir = new Teacher("Satyam", "Physics");
+        Teacher newSir = new Teacher();
+
+        System.out.println(satyamSir.getName());
+
+        System.out.println(newSir.getSubject());
+
+        newSir.setName("Bibhuti");
+        newSir.setSubject("Chemistry");
+
+        System.out.println(newSir.getSubject());
+
+        satyamSir.addSubjectTeacher(11, 'B');
+
+        System.out.println(satyamSir.isSubjectTeacher(10,'B'));
+        System.out.println(satyamSir.isSubjectTeacher(11,'B'));
+    }
+}
